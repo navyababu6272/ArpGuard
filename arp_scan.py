b8c1df22a2331c8367f9deb4f71231931837cf3b
Navya Babu <navyasree183@gmail.com>
2019-04-01 16:04:20 +0000

    phone vendor from other vendors!

--- arp_scan.py
+++ arp_scan.py
@@ -2,7 +2,11 @@
 
 # import the serious stuff
 import subprocess
+import urllib.request as urllib2
+import json
+import codecs
 
+url = "http://macvendors.co/api/"
 
 # the main function
 def main():
@@ -28,11 +32,24 @@ def main():
             print("\n")
             print("IP Address\t:", row[0])
             print("MAC ID\t\t:", row[1])
-            print("Manufacturer\t:", row[2])
-           
+            
+            
         else:
             break
-
+    for row in arp_list:
+        if len(row) != 0:
+            lst=row[1]
+            
+            
+            request = urllib2.Request(url+lst, headers={'User-Agent' : "API Browser"}) 
+            response = urllib2.urlopen( request )
+            reader = codecs.getreader("utf-8")
+            obj = json.load(reader(response))
+            print ("\n")
+            print (obj['result']['company']);
+   
+    #print company address
+            print (obj['result']['address']);
 
 # call the main function
 main()
