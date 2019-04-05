4fdc293e934c688310fb4a1dea3634af02f9b00b
Navya Babu <navyasree183@gmail.com>
2019-04-05 14:54:58 +0000

    mac files

--- arp_scan.py
+++ arp_scan.py
@@ -5,7 +5,7 @@ import subprocess
 import urllib.request as urllib2
 import json
 import codecs
-
+import re
 url = "http://macvendors.co/api/"
 
 # the main function
@@ -29,27 +29,40 @@ def main():
     # print the stuff
     for row in arp_list:
         if len(row) != 0:
-            print("\n")
+           
             print("IP Address\t:", row[0])
             print("MAC ID\t\t:", row[1])
+            print("\n")
             
             
         else:
             break
+           
     for row in arp_list:
         if len(row) != 0:
             lst=row[1]
-            
-            
             request = urllib2.Request(url+lst, headers={'User-Agent' : "API Browser"}) 
             response = urllib2.urlopen( request )
             reader = codecs.getreader("utf-8")
             obj = json.load(reader(response))
-            print ("\n")
-            print (obj['result']['company']);
-   
+            mac=(obj['result']['mac_prefix']);
+            vendor=(obj['result']['company']);
+            print("--------------------------------------------")
+            print(" MAC Prefix:",mac)
+            print("Vendor is:",vendor)
+            print("--------------------------------------------")
+            #print(type(c))
+            '''tinylist = ['Microsoft Corporation', 'Hewlett Packard']
+            for item in c:
+                for item1 in tinylist:
+                    if item == item1:
+                        print (item)
+                    else:
+                        print ("Not a mobile")'''
+ 
+    
     #print company address
-            print (obj['result']['address']);
+           #print (obj['result']['address']);
 
 # call the main function
 main()
