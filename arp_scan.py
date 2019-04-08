772ba32481aef1ea5d6601ce43a50694ae4735d8
Navya Babu <navyasree183@gmail.com>
2019-04-08 14:43:15 +0000

    modified scan

--- arp_scan.py
+++ arp_scan.py
@@ -32,18 +32,16 @@ def main():
     for row in arp_list:
         if len(row) != 0:
            
-            #print("IP Address\t:", row[0])
+            print("IP Address\t:", row[0])
             print("MAC ID\t\t:", row[1])
             print("\n")
-            
-            
         else:
             break
-    
-                 
-        
+    #mac Vendor lookup
     for row in arp_list:
+
         if len(row) != 0:
+
             lst=row[1]
             request = urllib2.Request(url+lst, headers={'User-Agent' : "API Browser"}) 
             response = urllib2.urlopen( request )
@@ -51,38 +49,35 @@ def main():
             obj = json.load(reader(response))
             mac=[obj['result']['mac_prefix']];
             vendor=(obj['result']['company']);
-            '''print("--------------------------------------------")
+
+            #printing mac prefix and vendor name
+            print("--------------------------------------------")
             print("MAC Prefix:",mac)
             print("Vendor:",vendor)
-            print("--------------------------------------------")'''
-            
+            print("--------------------------------------------")
+
+            #opening mobile_mac file  
             file = open("phone.txt","r")
             values=[]
-            lst=[]
+            mob=[]
             for line in file:
                 if len(line) != 0:
                     values = line.split()
-                    lst=[(values[0])]
+                    mob=[(values[0])]
                     
                 else:
                     break
-            for item in mac:
-                for item1 in lst:
+            #identifying mobiles connected in wifi
+
+            for item in mac:                     #mac contains list of mac_prefix of device connected to network
+                for item1 in mob:                #phone contains mac_prefixof all mobile phones
                     if item == item1:
-                        print ("is a mobile")
+                        print (item)
                     else:
                         print ("Not a mobile")        
-                    
+                 
         else:
             break
-      
-            
-           
-            
-          
-    
-    #print company address
-           #print (obj['result']['address']);
 
 # call the main function
 main()
