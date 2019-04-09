160e644a9122b44b413e3fe65becf9c624458255
Navya Babu <navyasree183@gmail.com>
2019-04-09 09:44:44 +0000

    loop corrected in arp_scan

--- arp_scan.py
+++ arp_scan.py
@@ -41,15 +41,15 @@ def main():
     for row in arp_list:
 
         if len(row) != 0:
-
+            phone=[]
             lst=row[1]
             request = urllib2.Request(url+lst, headers={'User-Agent' : "API Browser"}) 
             response = urllib2.urlopen( request )
             reader = codecs.getreader("utf-8")
             obj = json.load(reader(response))
-            mac=[obj['result']['mac_prefix']];
+            mac=(obj['result']['mac_prefix']);
             vendor=(obj['result']['company']);
-
+            phone=[mac]
             #printing mac prefix and vendor name
             print("--------------------------------------------")
             print("MAC Prefix:",mac)
@@ -57,24 +57,30 @@ def main():
             print("--------------------------------------------")
 
             #opening mobile_mac file  
-            file = open("phone.txt","r")
-            values=[]
-            mob=[]
-            for line in file:
-                if len(line) != 0:
-                    values = line.split()
-                    mob=[(values[0])]
-                    
-                else:
-                    break
-            #identifying mobiles connected in wifi
+            # define empty list
+            mob = []
+           
 
-            for item in mac:                     #mac contains list of mac_prefix of device connected to network
-                for item1 in mob:                #phone contains mac_prefixof all mobile phones
-                    if item == item1:
-                        print (item)
-                    else:
-                        print ("Not a mobile")        
+            # open file and read the content in a list
+            with open('phone.txt', 'r') as filehandle:  
+                filecontents = filehandle.readlines()
+
+                for line in filecontents:
+            # remove linebreak which is the last character of the string
+                    current_phone = line[:-1]
+
+            # add item to the list
+                    mob.append(current_phone)
+            
+                    
+                
+            #identifying mobiles connected in wifi
+                
+                for item in mob:                     #mac contains list of mac_prefix of device connected to network
+                    for item1 in phone:                #phone contains mac_prefixof all mobile phones
+                       if item == item1:
+                           print ("This is a mobile and he mac prefix is",item)
+                      
                  
         else:
             break
